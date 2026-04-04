eba67cb02890843a24f49404f4f786d8392cb422
MrBearing <takumi1988okamoto@gmail.com>
2026-04-04 09:19:13 +0900

    fix: keep ament keyword matching case-sensitive

--- lib/src/doc/ros.rs
+++ lib/src/doc/ros.rs
@@ -19,7 +19,7 @@ pub struct AmentTargetDependencies<'t> {
 
 impl<'t> RosCommand<'t> {
     pub fn from_raw(raw: &RawCommand<'t>) -> Option<Self> {
-        match raw.identifier.as_ref() {
+        match ascii_lowercase(raw.identifier.as_ref()).as_ref() {
             b"ament_package" => Some(Self::AmentPackage),
             b"catkin_package" => Some(Self::CatkinPackage),
             b"ament_target_dependencies" => {
@@ -40,10 +40,7 @@ impl<'t> RosCommand<'t> {
 }
 
 fn is_ament_keyword(token: &[u8]) -> bool {
-    matches!(
-        ascii_lowercase(token).as_ref(),
-        b"system" | b"public" | b"interface"
-    )
+    matches!(token, b"SYSTEM" | b"PUBLIC" | b"INTERFACE")
 }
 
 fn ascii_lowercase(bytes: &[u8]) -> Cow<'_, [u8]> {
